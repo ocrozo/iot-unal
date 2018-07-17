a847b75e2b3f53971132fb5938b9b9cce35123f9
ocrozo <ocrozo@gmail.com>
2018-07-17 06:14:19 -0500

    test controller rpi

--- om2m/README.md
+++ om2m/README.md
@@ -1,2 +1,14 @@
 # onem2m-demo
 oneM2M demonstration
+
+## Raspberry configuration
+You will connect the weather sensor as configured before and you will connect a LED to GPIO 4 with a resistor connected to a GND pin.
+Test your connections with the code `demo.1.py`in the sensors folder.
+
+### Install additional packages
+To connect to om2m you need a http client library: 
+```
+sudo apt-get install python-httplib2
+```
+
+
--- om2m/onem2m-device/controller_json.py
+++ om2m/onem2m-device/controller_json.py
@@ -4,70 +4,59 @@ import time
 import argparse
 import signal
 import sys
-import serial
-import simplejson as json
 import datetime 
 import io
 import os
+import httplib2
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import threading 
+import tsl2561
+import RPi.GPIO as GPIO
 
-FILENAME        = "/home/pi/values.json"
-VALUES          = json.loads('{"values": []}')
-MAX_FILE_LEN         = 45000
+# CSE params
+host = "10.10.4.109" # Computer IP address
+httpPort = 8080
 
-# send serial message 
-SERIALPORT = "/dev/ttyUSB0"
-BAUDRATE = 115200
-ser = serial.Serial()
+# AE params
+aeIP = "10.10.6.163" #Raspberry IP address
+aePort   = 80
+origin   = "Cae_device1"
 
 KEEP_RUNNING = True
 
 def keepRunning():
     return KEEP_RUNNING
 
-def initUART():        
-        # ser = serial.Serial(SERIALPORT, BAUDRATE)
-        ser.port=SERIALPORT
-        ser.baudrate=BAUDRATE
-        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-        ser.parity = serial.PARITY_NONE #set parity check: no parity
-        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-        ser.timeout = None          #block read
-
-        # ser.timeout = 0             #non-block read
-        # ser.timeout = 2              #timeout block read
-        ser.xonxoff = False     #disable software flow control
-        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-        #ser.writeTimeout = 0     #timeout for write
-        print 'Starting Up Serial Monitor'
-        try:
-                ser.open()
-        except serial.SerialException:
-                print("Serial {} port not available".format(SERIALPORT))
-                exit()
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        # self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def do_GET(self):
         self._set_headers()
-        #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-        saveJSON()
-        with open(FILENAME) as data_file:
-                self.wfile.write(data_file.read())
-    
+        # self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
     def do_HEAD(self):
         self._set_headers()
         
     def do_POST(self):
-        # Doesn't do anything with posted data
         self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        # self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        content_len = int(self.headers.getheader('content-length'))
+        post_body = self.rfile.read(content_len)
+        self.end_headers()
+        if "ON" in post_body:
+                print "LED on"
+                GPIO.output(4,GPIO.HIGH)
+        else:
+                print "LED off"
+                GPIO.output(4,GPIO.LOW)
+        data = json.loads(post_body)
+
+        self.wfile.write(data['foo'])
+        return
         
 def runWebServer(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
@@ -76,52 +65,75 @@ def runWebServer(server_class=HTTPServer, handler_class=S, port=80):
     while KEEP_RUNNING:
         #httpd.serve_forever()
         httpd.handle_request()
-def startupCheck():
-    if os.path.isfile(FILENAME):
-        print ("File exists and is readable")
-    else:
-        print ("File is missing, creating file...")
-        with open(FILENAME, 'w') as db_file:
-                db_file.write(json.dumps(VALUES))
-                db_file.close()
 
-def saveJSON():
-        with open(FILENAME, 'w') as fout:
-                        json.dump(VALUES, fout)
-                        print("Saved " + str(len(VALUES["values"])) + "entries in json file.")
-                        fout.close()
+def loop():
+
+    while True:
+        # Read luminosity values
+        
+        (light_ch0,light_ch1)= tsl2561.readValues()
+        print ("Full Spectrum(IR + Visible) :%d lux" % light_ch0)
+        print ("Infrared Value :%d lux" % light_ch1)
+        print ("Visible Value :%d lux" % (light_ch0 - light_ch1))	
+        time.sleep(5)
+
+# Set output mode for GPIO 4 to OUT
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(4,GPIO.OUT)
+
+def setup_ae():
+    # Create AE resource
+    result = send("/server",2,"{\"m2m:ae\":{\"rn\":\"mydevice1\",\"api\":\"mydevice1.company.com\",\"rr\":\"true\",\"poa\":[\"http://"+aeIP+":"+aePort+"\"]}}")
+  
+    if result=="HTTP/1.1 201 Created":
+        # Create Container resource
+        send("/server/mydevice1",3,"{\"m2m:cnt\":{\"rn\":\"luminosity\"}}")
+
+        #Create ContentInstance resource
+        send("/server/mydevice1/luminosity",4,"{\"m2m:cin\":{\"con\":\"0\"}}")
+
+        # Create Container resource
+        send("/server/mydevice1",3,"{\"m2m:cnt\":{\"rn\":\"led\"}}")
+
+        # Create ContentInstance resource
+        send("/server/mydevice1/led",4,"{\"m2m:cin\":{\"con\":\"OFF\"}}")
+
+        # Create Subscription resource
+        send("/server/mydevice1/led",23,"{\"m2m:sub\":{\"rn\":\"led_sub\",\"nu\":[\"Cae_device1\"],\"nct\":1}}")
+  
+
+# Method in charge of sending request to the CSE
+def send(url, ty, rep) {
+
+    heads = {"Host":host,
+        "X-M2M-Origin":origin,
+        "Content-Type": "application/json;ty="+ty}
+    
+    h = httplib2.Http(".cache")
+    (resp, content) = h.request("http://" + host + "~" + url, "POST", headers=heads, body=rep)
+    print(resp)
+    return resp
+}
 
 # Main program logic follows:
 if __name__ == '__main__':
-        initUART()
-        startupCheck()
-        thread= threading.Thread(target=runWebServer)
-        thread.start()
-        #f= open(FILENAME,"a")
-        with open(FILENAME, 'r') as fin:
-                VALUES = json.load(fin)
-                fin.close()
+    # Setup pins
+    setup_gpio()
 
-        
-        print ('Press Ctrl-C to quit.')
-        try:
-                while ser.isOpen() : 
-                        # time.sleep(100)
-                        if (ser.inWaiting() > 0): # if incoming bytes are waiting 
-                                data_str = ser.readline().strip().strip('\x00').strip() 
-                                # print(repr(data_str))
-				json_data = json.loads(data_str)
-                                json_data["date"] = unicode(datetime.datetime.now())
-                                jstr = json.dumps(json_data, indent=4)
-                                VALUES["values"].append(json_data)
-                                if len(VALUES["values"]) > MAX_FILE_LEN :
-                                        VALUES["values"].pop(0)
-                                # f.write(jstr)
-                                print(jstr)
-        except (KeyboardInterrupt, SystemExit):
-                saveJSON()
-                ser.close()
-                KEEP_RUNNING=False
-                thread.kill_received=True                 
-                # os._exit(1)
-                exit()
+    # Start web server
+    thread= threading.Thread(target=runWebServer)
+    thread.start()
+
+    # Setup AE ressources
+    setup_ae()
+
+    print ('Press Ctrl-C to quit.')
+    try:
+        loop()
+    except (KeyboardInterrupt, SystemExit):
+        KEEP_RUNNING=False
+        thread.kill_received=True                 
+        # os._exit(1)
+        exit()
